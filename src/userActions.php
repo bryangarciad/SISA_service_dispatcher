ceb78e88217b2d76ae693a88ee33643fdb04720d
Bryan Ramses Garcia <bryan.garcia.duran@gmail.com>
2022-01-29 22:59:11 -0700

    sfdf

--- src/userActions.php
+++ src/userActions.php
@@ -15,8 +15,9 @@ class user extends action {
     public function create ($userName, $password, $site_id, $rol) {
         $query = sprintf('SELECT  COUNT(*) FROM user WHERE user_name = "%s"', $userName);
         $results = $this->mysqli->query($query);
-        echo var_dump($results);
-        
+        $row = $result->fetch_array(MYSQLI_NUM);
+        echo var_dump($row);
+
         $query = sprintf('INSERT INTO `user`( `user_name`, `password`, `site_id`, `rol`) VALUES ("%s", "%s", %d, "%s")', $userName, $password, $site_id, $rol);
         $results = $this->mysqli->query($query);
         echo var_dump($results);
