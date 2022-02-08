71948a78c53095570f0db92c3525c788be1b0e5e
Bryan Ramses Garcia <bryan.garcia.duran@gmail.com>
2022-02-07 21:53:06 -0700

    sdfsdfdsf

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ if (!key_exists('action', $_POST)  || ! $_POST['action']) {
 // My SQL init
 
 
-$mysqli = new mysqli("localhost", 'root', '', 'sisa');
+// $mysqli = new mysqli("localhost", 'root', '', 'sisa');
 $mysqli = new mysqli("localhost", 'circuitc_root', 'Elisa1121*', 'circuit_sisa');
 
 
