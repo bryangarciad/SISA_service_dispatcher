4c75d91ee35fbea3cd2be0e91e2bcbc51fed5baa
Bryan Ramses Garcia <bryan.garcia.duran@gmail.com>
2022-02-07 22:26:04 -0700

    asdasdas

--- src/authenticationService.php
+++ src/authenticationService.php
@@ -17,11 +17,13 @@ class Authentication {
     public function login ($data) {
         $userName = $data['user_name'];
         $password = $data['password'];
-        $query = sprintf('SELECT  COUNT(*) FROM user WHERE user_name = "%s" AND password = "%s"', $userName, $password);
+        $query = sprintf('SELECT  * FROM user WHERE user_name = "%s" AND password = "%s"', $userName, $password);
         $results = $this->mysqli->query($query);
         $row = $results->fetch_row();
 
-        if(intval($row[0]) > 0) {
+        echo var_dump($row);
+
+        if($row[0] > 0) {
             $_SESSION['token'] = sha1($userName . $password);
             response::sendOk(['msg' => 'ok']);
         } else {
